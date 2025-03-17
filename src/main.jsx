72e938b90bf21c635a00750e6169f9d2626d426f
isakhb <143383625+isakhb@users.noreply.github.com>
2025-03-17 04:11:59 +0100

    Kommentar i main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+//Gjemmer standard-styling, slik at min css/styling kommer fram. Samme i app.jsx
 //import './index.css'
 import App from './App.jsx'
 
